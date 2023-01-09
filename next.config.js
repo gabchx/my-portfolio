ee9f1f9255a9d25b57808524171c746e6caa8fab
Gabriel C <gabriel.chaiix@gmail.com>
2023-01-10 00:33:48 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   /**
    * Tell Next.js where the `public` folder is.
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
-  assetPrefix: isProd ? "/my-portfolio/" : "",
+  assetPrefix: isProd ? '/my-portfolio/' : '',
   /**
    * Disable server-based image optimization.
    *
