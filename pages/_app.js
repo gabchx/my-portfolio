10218d69af425b06d21b09bae50bed6a11abff48
Gabriel C <gabriel.chaiix@gmail.com>
2023-01-10 22:03:08 +0900

    d-1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import "../styles/globals.css";
+import '../styles/globals.css';
+import React from 'react';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {' '}
+      <Component {...pageProps} />
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-KYYXVFSRH5"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {
+          "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-KYYXVFSRH5');"
+        }
+      </Script>
+      ;
+    </>
+  );
 }
 
 export default MyApp;
