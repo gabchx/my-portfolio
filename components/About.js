f784e07cc8936298ef4b085ef152377c0b8cc692
Gabriel <83697171+gabchx@users.noreply.github.com>
2023-11-07 13:01:45 +0100

    maj wip

--- components/About.js
+++ components/About.js
@@ -6,7 +6,7 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Gabriel.&thinsp;
+            Work in progress... <br></br>Hi, I'm Gabriel.&thinsp;
             <br className="hidden lg:inline-block" />I love to build amazing
             wind turbine.
           </h1>
@@ -23,7 +23,7 @@ export default function About() {
               Work With Me
             </a>
             <a
-              href="#projects"
+              href="#projects" ou
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
             >
               See My Past Work
